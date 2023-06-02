44cc85e8668140de54f9fc8d2bae8ac42307c2eb
KhanLewis <khanlewis@hotmail.co.uk>
2023-06-02 12:05:20 +0000

    added cart models and cart views aswell as urls

--- cart/models.py
+++ cart/models.py
@@ -1,4 +1,7 @@
 from django.db import models
+from django.contrib.auth.models import User
+from products.models import Product
+
 
 class Cart(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
@@ -16,3 +19,14 @@ class CartItem(models.Model):
     cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     quantity = models.PositiveIntegerField(default=1)
+
+
+class Order(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    order_number = models.CharField(max_length=10, unique=True)
+    date_created = models.DateTimeField(auto_now_add=True)
+    total_amount = models.DecimalField(max_digits=10, decimal_places=2)
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"Order {self.order_number} by {self.user.username}"
--- cart/views.py
+++ cart/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import get_object_or_404, redirect, render
 from django.views import View
 from .models import Cart, CartItem, Product
 
+
 class AddToCartView(View):
     def post(self, request, product_id):
         cart, created = Cart.objects.get_or_create(user=request.user)
@@ -14,3 +15,24 @@ class AddToCartView(View):
             cart_item.save()
 
         return redirect('cart:cart_detail')
+
+
+class RemoveFromCartView(View):
+    def post(self, request, cart_item_id):
+        cart_item = CartItem.objects.get(id=cart_item_id)
+        cart_item.delete()
+
+        return redirect('cart:cart_detail')
+
+
+class CartDetailView(View):
+    def get(self, request):
+        cart = Cart.objects.get(user=request.user)
+        cart_items = CartItem.objects.filter(cart=cart)
+
+        context = {
+            'cart': cart,
+            'cart_items': cart_items
+        }
+
+        return render(request, 'cart/cart_detail.html', context)
